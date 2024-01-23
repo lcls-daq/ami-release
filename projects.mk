02f8d16adc576ddb966523ea5482797d995cd135
Daniel Damiani <ddamiani@slac.stanford.edu>
2024-01-22 20:36:18 -0800

    update from /reg/g to /cds/group for paths

--- projects.mk
+++ projects.mk
@@ -8,30 +8,30 @@ rprojects := $(wildcard *)
 #
 #  List external package base directories for convenience
 #
-epics_use      := /reg/common/package/epicsca/3.14.12
-evgr_use       := /reg/g/pcds/package/external/evgr_V00-00-05
-qt_use         := /reg/common/package/qt/4.8.4
-qwt_use        := /reg/g/pcds/package/external/qwt-5.1.1-wfopt-logfix
-python_use     := /reg/common/package/python/2.7.16
-python3_use    := /reg/common/package/python/3.6.8
-libraw1394_use := /reg/g/pcds/package/external/libdc1394
-libdc1394_use  := /reg/g/pcds/package/external/libdc1394
-offlinedb_use  := /reg/g/pcds/package/external/offlinedb-1.5.1
-edt_use        := /reg/g/pcds/package/external/edt
-leutron_use    := /reg/g/pcds/package/external/leutron_V00-00-00
-fli_use        := /reg/g/pcds/package/external/fli-dist-1.71
-andor_use      := /reg/g/pcds/package/external/andor-2.95.30003.0
-libusb_use     := /reg/g/pcds/package/external/libusb-1.0.0
-usdusb4_use    := /reg/g/pcds/package/external/usdusb4
-acqiris_use    := /reg/g/pcds/package/external/acqiris_3.3a
-relaxd_use     := /reg/g/pcds/package/external/relaxd-1.9.9
-pvcam_use      := /reg/g/pcds/package/external/pvcam2.7.1.7
-picam_use      := /reg/g/pcds/package/external/picam-2.6.1
-gsl_use        := /reg/g/pcds/package/external/gsl-2.7.1
-boost_use      := /reg/g/pcds/pkg_mgr/release/boost/1.63.0
-ndarray_use    := /reg/common/package/ndarray/1.1.8
-psalg_use      := /reg/common/package/psalg/1.0.12
-pdsdata_use    := /reg/common/package/pdsdata/10.1.0
+epics_use      := /cds/sw/package/epicsca/3.14.12
+evgr_use       := /cds/group/pcds/package/external/evgr_V00-00-05
+qt_use         := /cds/sw/package/qt/4.8.4
+qwt_use        := /cds/group/pcds/package/external/qwt-5.1.1-wfopt-logfix
+python_use     := /cds/sw/package/python/2.7.16
+python3_use    := /cds/sw/package/python/3.6.8
+libraw1394_use := /cds/group/pcds/package/external/libdc1394
+libdc1394_use  := /cds/group/pcds/package/external/libdc1394
+offlinedb_use  := /cds/group/pcds/package/external/offlinedb-1.5.1
+edt_use        := /cds/group/pcds/package/external/edt
+leutron_use    := /cds/group/pcds/package/external/leutron_V00-00-00
+fli_use        := /cds/group/pcds/package/external/fli-dist-1.71
+andor_use      := /cds/group/pcds/package/external/andor-2.95.30003.0
+libusb_use     := /cds/group/pcds/package/external/libusb-1.0.0
+usdusb4_use    := /cds/group/pcds/package/external/usdusb4
+acqiris_use    := /cds/group/pcds/package/external/acqiris_3.3a
+relaxd_use     := /cds/group/pcds/package/external/relaxd-1.9.9
+pvcam_use      := /cds/group/pcds/package/external/pvcam2.7.1.7
+picam_use      := /cds/group/pcds/package/external/picam-2.6.1
+gsl_use        := /cds/group/pcds/package/external/gsl-2.7.1
+boost_use      := /cds/group/pcds/pkg_mgr/release/boost/1.63.0
+ndarray_use    := /cds/sw/package/ndarray/1.1.8
+psalg_use      := /cds/sw/package/psalg/1.0.12
+pdsdata_use    := /cds/sw/package/pdsdata/10.1.0
 
 
 #
@@ -74,7 +74,7 @@ projects += \
       epics \
       qt
 
-timetool_use            := /reg/g/pcds/dist/pds/10.6.9-p9.3.4/build/timetool
+timetool_use            := /cds/group/pcds/dist/pds/10.8.1-p10.1.0/build/timetool
 timetool_use_include    := $(timetool_use)/include
 timetool_use_lib_x86_64 := $(timetool_use)/lib/x86_64-linux-opt
 timetool_use_lib_rhel6  := $(timetool_use)/lib/x86_64-linux-opt
