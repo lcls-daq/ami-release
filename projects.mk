61494f456eb6bbb4e21589648fc6e3b6478386ba
Daniel Damiani <ddamiani@slac.stanford.edu>
2024-10-07 21:20:29 -0700

    add rocky9 support to makefiles

--- projects.mk
+++ projects.mk
@@ -8,7 +8,7 @@ rprojects := $(wildcard *)
 #
 #  List external package base directories for convenience
 #
-epics_use      := /cds/sw/package/epicsca/3.14.12
+epics_use      := /cds/group/pcds/package/external/epicsca-R3.14.12-0.4.0
 evgr_use       := /cds/group/pcds/package/external/evgr_V00-00-05
 qt_use         := /cds/sw/package/qt/4.8.4
 qwt_use        := /cds/group/pcds/package/external/qwt-5.1.1-wfopt-logfix
@@ -29,7 +29,7 @@ pvcam_use      := /cds/group/pcds/package/external/pvcam2.7.1.7
 picam_use      := /cds/group/pcds/package/external/picam-2.6.1
 gsl_use        := /cds/group/pcds/package/external/gsl-2.7.1
 boost_use      := /cds/group/pcds/pkg_mgr/release/boost/1.63.0
-ndarray_use    := /cds/sw/package/ndarray/1.1.8
+ndarray_use    := /cds/sw/package/ndarray/1.1.12
 psalg_use      := /cds/sw/package/psalg/1.0.12
 pdsdata_use    := /cds/sw/package/pdsdata/10.1.1
 
@@ -46,6 +46,7 @@ boost_use_include    := $(boost_use)/linux-x86_64/include
 boost_use_lib_x86_64 := $(boost_use)/linux-x86_64/lib
 boost_use_lib_rhel6  := $(boost_use)/rhel6-x86_64/lib
 boost_use_lib_rhel7  := $(boost_use)/rhel7-x86_64/lib
+boost_use_lib_rhel9  := $(boost_use)/rhel9-x86_64/lib
 
 ndarray_use_include := $(ndarray_use)
 
@@ -54,12 +55,14 @@ psalg_use_i386   := $(psalg_use)/i386-linux
 psalg_use_x86_64 := $(psalg_use)/x86_64-linux
 psalg_use_rhel6  := $(psalg_use)/x86_64-rhel6
 psalg_use_rhel7  := $(psalg_use)/x86_64-rhel7
+psalg_use_rhel9  := $(psalg_use)/x86_64-rhel9
 
 pdsdata_use_include:= $(pdsdata_use)/x86_64-rhel7-opt
 pdsdata_use_i386   := $(pdsdata_use)/i386-linux
 pdsdata_use_x86_64 := $(pdsdata_use)/x86_64-linux
 pdsdata_use_rhel6  := $(pdsdata_use)/x86_64-rhel6
 pdsdata_use_rhel7  := $(pdsdata_use)/x86_64-rhel7
+pdsdata_use_rhel9  := $(pdsdata_use)/x86_64-rhel9
 
 projects :=
 
@@ -74,11 +77,12 @@ projects += \
       epics \
       qt
 
-timetool_use            := /cds/group/pcds/dist/pds/10.10.1-p10.1.1/build/timetool
+timetool_use            := /cds/group/pcds/dist/pds/10.10.2-p10.1.1/build/timetool
 timetool_use_include    := $(timetool_use)/include
 timetool_use_lib_x86_64 := $(timetool_use)/lib/x86_64-linux-opt
 timetool_use_lib_rhel6  := $(timetool_use)/lib/x86_64-linux-opt
 timetool_use_lib_rhel7  := $(timetool_use)/lib/x86_64-rhel7-opt
+timetool_use_lib_rhel9  := $(timetool_use)/lib/x86_64-rhel9-opt
 
 projects += timetool
 
